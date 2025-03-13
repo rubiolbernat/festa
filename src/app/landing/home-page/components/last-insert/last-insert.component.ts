871d75d6fed29a27e45191192c0a7a1327266c84
BR <rubiolbernat@gmail.com>
2025-03-13 12:31:48 +0100

    conffetti

--- src/app/landing/home-page/components/last-insert/last-insert.component.ts
+++ src/app/landing/home-page/components/last-insert/last-insert.component.ts
@@ -1,10 +1,11 @@
 import { DrinkingDataService } from './../../../../core/services/drinking-data/drinking-data.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { DatePipe, NgIf } from '@angular/common';
 import { ConcertService } from '../../../../core/services/concert_db/concert.service';
 import { CombinedDrinkUserData } from '../../../../core/models/drink-data.model';
 import { Concert } from '../../../../core/models/concert';
 import { RouterModule } from '@angular/router';
+import JSConfetti from 'js-confetti'
 
 @Component({
   selector: 'app-last-insert',
@@ -13,16 +14,21 @@ import { RouterModule } from '@angular/router';
   templateUrl: './last-insert.component.html',
   styleUrl: './last-insert.component.css'
 })
-export class LastInsertComponent implements OnInit {
+export class LastInsertComponent implements OnInit, AfterViewInit {
   nextConcert: Concert | null = null;
   lastDrink: CombinedDrinkUserData | null = null;
-
+  jsConfetti: any;
 
   constructor(private concertService: ConcertService, private drinkingdataService: DrinkingDataService) { }
 
   ngOnInit(): void {
     this.loadNextConcert();
     this.loadlastinserted();
+  }
+  ngAfterViewInit(): void {
+    this.jsConfetti = new JSConfetti({
+      canvas: document.getElementById('confetti-canvas') as HTMLCanvasElement
+    });
   }
 
   loadNextConcert(): void {
@@ -39,4 +45,19 @@ export class LastInsertComponent implements OnInit {
     });
   }
 
+  lancarConfetti() {
+    if (this.jsConfetti) {
+      // Llança confeti de colors
+      this.jsConfetti.addConfetti({
+        confettiNumber: 170, // Ajusta el número segons sigui necessari
+      });
+
+      // Llança emojis
+      this.jsConfetti.addConfetti({
+        emojis: ['🍺', '🥂', '🍷', '🥃', '🍻', '🍸', '🍾'],
+        emojiSize: 30,
+        confettiNumber: 20, // Ajusta el número segons sigui necessari
+      });
+    }
+  }
 }
