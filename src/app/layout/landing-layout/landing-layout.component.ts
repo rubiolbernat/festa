e406897fd034be7e2e3b055e3eaa377b3b952b0f
BR <rubiolbernat@gmail.com>
2025-03-14 13:57:10 +0100

    eliminar loginoverlay

--- src/app/layout/landing-layout/landing-layout.component.ts
+++ src/app/layout/landing-layout/landing-layout.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginOverlayComponent } from '../../landing/login/login-overlay.component';
 import { HeaderComponent } from '../../landing/components/header/header.component';
 import { FooterComponent } from '../../landing/components/footer/footer.component';
 
 @Component({
   selector: 'app-landing-layout',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginOverlayComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './landing-layout.component.html',
   styleUrl: './landing-layout.component.css'
 })
