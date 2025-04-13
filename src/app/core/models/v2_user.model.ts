c1f4ac1b38867c656b48547e1454d2a35ddb31f8
BR <rubiolbernat@gmail.com>
2025-04-13 22:07:42 +0200

    refactor: remove redundant User interface and update UserSimplified structure

--- src/app/core/models/v2_user.model.ts
+++ src/app/core/models/v2_user.model.ts
@@ -15,20 +15,3 @@ export interface UserSimplified {
   following?: boolean; // Indica si l'usuari està seguint a l'usuari actual
   follows_you?: number; // Nombre de seguidors de l'usuari
 }
-
-export interface User {
-  user_id: number;
-  name: string;
-  email: string;
-  password?: string; // No retornarem la contrasenya per seguretat
-  created_at?: string;
-  refresh_token?: string;
-  role: string;
-}
-
-export interface UserSimplified {
-  user_id: number;
-  id: number;
-  name: string;
-  role: string;
-}
